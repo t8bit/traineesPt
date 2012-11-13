c6d576dd8c19c7741d2f23d94e1b30b07a451571
Tiago André <geral.t8bit@gmail.com>
2012-11-13 16:22:55 +0000

    all

---
+++ config/fbconfig.php
@@ -0,0 +1,9 @@
+<?php
+
+
+
+define('APP_ID', '449459288425059');
+define('APP_SECRET', '8b07d204692e62ab3443fec5a2c4717c');
+
+
+?>
---
+++ send.php
@@ -0,0 +1,3 @@
+<?php
+shell_exec("php videoManipulator.class.php >/dev/null 2>/dev/null &");
+?>
